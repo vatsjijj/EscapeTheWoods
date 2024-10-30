40a10a25b0e2ec1cc94066a4b3f7fb0e2b4fb098
vtj <patricoconnor12@gmail.com>
2024-10-30 14:03:19 -0400

    Update entity

--- src/main/java/org/etw/EscapeTheWoods/Entity.java
+++ src/main/java/org/etw/EscapeTheWoods/Entity.java
@@ -28,7 +28,40 @@ public class Entity {
         this.position = position;
     }
 
+    public boolean attack(Entity entity) {
+        return entity.damage(damage);
+    }
+
+    public boolean damage(int amount) {
+        // This needs to have a random chance but
+        // for now we'll pretend that it always
+        // works.
+        this.health -= amount;
+        return true;
+    }
+
+    public boolean die() {
+        this.health = 0;
+        return true;
+    }
+
     public void setPosition(Room position) {
         this.position = position;
+    }
+
+    public Room getPosition() {
+        return this.position;
+    }
+
+    public double getHealth() {
+        return this.health;
+    }
+
+    public double getStamina() {
+        return this.stamina;
+    }
+
+    public boolean isDead() {
+        return this.health <= 0.0;
     }
 }
