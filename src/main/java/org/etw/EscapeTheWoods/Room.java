e93aa9e2acf744d153a1c880062ef092b7ff372f
Sydney McGreal <mcgreasm@miamioh.edu>
2024-10-30 14:55:31 -0400

    room class

--- src/main/java/org/etw/EscapeTheWoods/Room.java
+++ src/main/java/org/etw/EscapeTheWoods/Room.java
@@ -4,5 +4,10 @@ public class Room {
     private double difficulty;
     private String description;
 
+    public Room(double difficulty,String description){
+        this.difficulty = difficulty;
+        this.description = description;
+    }
+
 
 }
