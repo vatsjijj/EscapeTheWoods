0e38588bcc134bdfe15afdb0cf654c183b907f09
vtj <patricoconnor12@gmail.com>
2024-10-30 13:40:44 -0400

    Add placeholder room class
    And update Entity

--- src/main/java/org/etw/EscapeTheWoods/Entity.java
+++ src/main/java/org/etw/EscapeTheWoods/Entity.java
@@ -1,5 +1,9 @@
 package org.etw.EscapeTheWoods;
 
 public class Entity {
-    
+    private double stamina;
+    private double health;
+    private int damage;
+    private int speed;
+    private Room position;
 }
