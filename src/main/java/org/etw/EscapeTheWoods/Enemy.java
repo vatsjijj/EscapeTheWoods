1432a4d96922b438ad35fe9163f3b08372b10ef4
Brandon Belfiore <belfiobj@miamioh.edu>
2024-11-01 13:37:32 -0400

    Adding methods to Enemy

--- src/main/java/org/etw/EscapeTheWoods/Enemy.java
+++ src/main/java/org/etw/EscapeTheWoods/Enemy.java
@@ -2,5 +2,13 @@
 package org.etw.EscapeTheWoods;
 
 public class Enemy extends Entity {
-    
+
+    public boolean retreat() {
+
+        return false;
+    }
+
+    public int specialAttack() {
+        
+    }
 }
