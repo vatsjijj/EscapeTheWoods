5d4de29aacb30ef86e0282298db2d5b6f87a0785
Sydney McGreal <mcgreasm@miamioh.edu>
2024-11-01 13:28:27 -0400

    attributes to Item

--- src/main/java/org/etw/EscapeTheWoods/Item.java
+++ src/main/java/org/etw/EscapeTheWoods/Item.java
@@ -2,4 +2,13 @@ package org.etw.EscapeTheWoods;
 
 public class Item {
 
+    private String name;
+    private String description;
+    private int rarity;
+
+    public Item(String name, String description, int rarity){
+        this.name = name;
+        this.description = description;
+        this.rarity = rarity;
+    }
 }
