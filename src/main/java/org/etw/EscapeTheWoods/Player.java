f6075f7faf8f175d785dd5e1044cbd6fc8c0c4d5
vtj <patricoconnor12@gmail.com>
2024-11-01 13:17:47 -0400

    Add stuff

--- src/main/java/org/etw/EscapeTheWoods/Player.java
+++ src/main/java/org/etw/EscapeTheWoods/Player.java
@@ -1,6 +1,30 @@
 package org.etw.EscapeTheWoods;
 
-// Supposed to extend on Entity.
-public class Player {
-    
+import java.util.ArrayList;
+
+public class Player extends Entity {
+    private ArrayList<Item> inventory;
+    private double temperature;
+    private double thirst;
+    private double hunger;
+    private int score;
+
+    public Player() {
+        super(100, 100, 1, 20);
+        this.inventory = new ArrayList<>();
+        this.temperature = 98.6;
+        this.thirst = 0;
+        this.hunger = 0;
+        this.score = 0;
+    }
+
+    public boolean eat(Food food) {
+        // Need to expand on the Food class.
+        return true;
+    }
+
+    public boolean drink(Food drink) {
+        // Need to expand on the Food class.
+        return true;
+    }
 }
