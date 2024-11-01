2e70971abb7a2a5fb26418dfb59651fa4310c151
Brandon Belfiore <belfiobj@miamioh.edu>
2024-11-01 14:10:15 -0400

    Start Implementing  retreat()

--- src/main/java/org/etw/EscapeTheWoods/Enemy.java
+++ src/main/java/org/etw/EscapeTheWoods/Enemy.java
@@ -2,13 +2,17 @@
 package org.etw.EscapeTheWoods;
 
 public class Enemy extends Entity {
-    
+
     public Enemy(double health, double stamina, int damage, int speed) {
         super(health, stamina, damage, speed);
     }
 
     public boolean retreat() {
-        
+        double health = getHealth();
+        if (health <= health / 2) {
+            
+        }
+
         return false;
     }
 
