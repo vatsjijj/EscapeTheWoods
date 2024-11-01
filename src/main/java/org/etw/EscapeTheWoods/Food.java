0fdb2c6e1e595b3e970c66906bcfa22fb25ed499
Sydney McGreal <mcgreasm@miamioh.edu>
2024-11-01 13:41:07 -0400

    food super

--- src/main/java/org/etw/EscapeTheWoods/Food.java
+++ src/main/java/org/etw/EscapeTheWoods/Food.java
@@ -6,7 +6,7 @@ public class Food extends Item{
 
 
     public Food(String name, String description, int rarity){
-        super(name,description,rarity)
+        super(name,description,rarity);
     }
 
 }
