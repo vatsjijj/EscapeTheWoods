50d0ad8be4c356a984610a5cd07dffea7e9a39d5
Gerry <bennetg5@miamioh.edu>
2024-11-01 13:34:48 -0400

    Weapons update

--- src/main/java/org/etw/EscapeTheWoods/Weapons.java
+++ src/main/java/org/etw/EscapeTheWoods/Weapons.java
@@ -1,5 +1,9 @@
 package org.etw.EscapeTheWoods;
-
+//Inherits Item class
 public class Weapons {
+private int DamageMultiplyer;
 
+    public Weapons(int DamageMultiplyer){ 
+        this.DamageMultiplyer = DamageMultiplyer;
+    }
 }
