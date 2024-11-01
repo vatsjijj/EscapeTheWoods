524c16c521494d558fb2500da5d5ffb0f5e2f59d
Sydney McGreal <mcgreasm@miamioh.edu>
2024-11-01 13:33:53 -0400

    attribute for food

--- src/main/java/org/etw/EscapeTheWoods/Food.java
+++ src/main/java/org/etw/EscapeTheWoods/Food.java
@@ -1,5 +1,12 @@
 package org.etw.EscapeTheWoods;
 
-public class Food {
+public class Food extends Item{
+
+    private int quantity;
+
+
+    public Food(String name, String description, int rarity){
+        super(name,description,rarity)
+    }
 
 }
