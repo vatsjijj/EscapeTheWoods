029476fc0fbbc1bbc8d04bdbc174595376209845
Brandon Belfiore <belfiobj@miamioh.edu>
2024-11-01 14:14:55 -0400

    Implemented retreat() with Java.util.Random

--- src/main/java/org/etw/EscapeTheWoods/Enemy.java
+++ src/main/java/org/etw/EscapeTheWoods/Enemy.java
@@ -1,6 +1,8 @@
 
 package org.etw.EscapeTheWoods;
 
+import java.util.Random;
+
 public class Enemy extends Entity {
 
     public Enemy(double health, double stamina, int damage, int speed) {
@@ -10,9 +12,14 @@ public class Enemy extends Entity {
     public boolean retreat() {
         double health = getHealth();
         if (health <= health / 2) {
-            
-        }
+            Random rand = new Random();
+            int rand_int = rand.nextInt(100);
+            if (rand_int > 80) {
+                return true;
+            }
 
+        }
+        
         return false;
     }
 
