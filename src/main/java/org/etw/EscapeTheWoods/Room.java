f39d4e4e32d10a13463eea69c104c88782bd0eb1
Sydney McGreal <mcgreasm@miamioh.edu>
2024-11-03 15:27:08 -0500

    room comments

--- src/main/java/org/etw/EscapeTheWoods/Room.java
+++ src/main/java/org/etw/EscapeTheWoods/Room.java
@@ -1,11 +1,29 @@
+/**
+ * Class: Room
+ * @author Sydney McGreal
+ * @version 1.0
+ * Course: CSE 201 Spring 2024
+ * Written: October 31, 2024
+ * 
+ * Purpose: This class represents the Room application for the Escape the Woods project
+ */
+
 package org.etw.EscapeTheWoods;
 
+
 public class Room {
+    // attributes
     private double difficulty;
     private String description;
 
+    /**
+     * Room Constructor
+     * @param difficulty
+     * @param description
+     */
     public Room(double difficulty,String description){
         this.difficulty = difficulty;
         this.description = description;
     }
-}
+
+} // end of class
