948f44af182e306cac14df715274c24b4f60e447
Sydney McGreal <mcgreasm@miamioh.edu>
2024-11-03 15:29:04 -0500

    weapons comments

--- src/main/java/org/etw/EscapeTheWoods/Weapons.java
+++ src/main/java/org/etw/EscapeTheWoods/Weapons.java
@@ -1,8 +1,26 @@
+/**
+ * Class: Room
+ * @author Gregg Bennett
+ * @version 1.0
+ * Course: CSE 201 Spring 2024
+ * Written: October 31, 2024
+ * 
+ * Purpose: This class represents the Weapons application for the Escape the Woods project
+ */
+
 package org.etw.EscapeTheWoods;
 //Inherits Item class
 public class Weapons extends Item{
-private int DamageMultiplyer;
+    // attributes
+    private int DamageMultiplyer;
 
+    /**
+     * Weapons Constructor
+     * @param name
+     * @param description
+     * @param rarity
+     * @param DamageMultiplyer
+     */
     public Weapons(String name, String description, int rarity, int DamageMultiplyer){ 
         super(name, description, rarity);
         this.DamageMultiplyer = DamageMultiplyer;
