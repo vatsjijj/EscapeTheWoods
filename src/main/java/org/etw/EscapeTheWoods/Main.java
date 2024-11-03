3e66492cefaabb0dd3f8348137aa5977ea031f2f
Brandon Belfiore <belfiobj@miamioh.edu>
2024-11-03 17:38:12 -0500

    Started implementing Main switch case

--- src/main/java/org/etw/EscapeTheWoods/Main.java
+++ src/main/java/org/etw/EscapeTheWoods/Main.java
@@ -1,6 +1,7 @@
 package org.etw.EscapeTheWoods;
 
 import java.util.Scanner;
+
 /**
  * The Main Method of the Escape The Woods game.
  * This is where the code will run.
@@ -10,7 +11,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Escape the Woods.");
-        System.out.println();
+        System.out.println("Type help for a list of commands.");
+        System.out.println("Would you like to start the game? [Y]/[N]");
+        
+        Scanner userChoice = new Scanner(System.in);
+        String yesOrNo = userChoice.nextLine();
+        switch(yesOrNo) {
+            case "Y":
 
+                break;
+            case "N":
+
+                break;
+            default:
+                
+                break;
+        }
+
+        userChoice.close();
     }
 }
