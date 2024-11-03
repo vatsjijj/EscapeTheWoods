084ca3876d8129733b79606937d4cb620139888b
Brandon Belfiore <belfiobj@miamioh.edu>
2024-11-03 17:28:11 -0500

    Importing Scanner into Main

--- src/main/java/org/etw/EscapeTheWoods/Main.java
+++ src/main/java/org/etw/EscapeTheWoods/Main.java
@@ -1,16 +1,16 @@
 package org.etw.EscapeTheWoods;
 
+import java.util.Scanner;
 /**
  * The Main Method of the Escape The Woods game.
  * This is where the code will run.
- * Right now it just tests the classes. 
+ * Right now it just tests the classes and has 
+ * a very basic UI.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
-
-        // Visualizing enemy bear
-        // Enemy bear = new Enemy(100, 100, 25, 30);
+        System.out.println("Welcome to Escape the Woods.");
+        System.out.println();
 
     }
 }
