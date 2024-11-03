a7da9b203be30d57f9a0fda58f6bf796a9b2c61f
Brandon Belfiore <belfiobj@miamioh.edu>
2024-11-03 17:08:49 -0500

    Comment on Main

--- src/main/java/org/etw/EscapeTheWoods/Main.java
+++ src/main/java/org/etw/EscapeTheWoods/Main.java
@@ -1,7 +1,15 @@
 package org.etw.EscapeTheWoods;
 
+/**
+ * The Main Method of the Escape The Woods game.
+ * This is where the code will run.
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+
+        // Visualizing enemy bear
+        // Enemy bear = new Enemy(100, 100, 25, 30);
+
     }
 }
