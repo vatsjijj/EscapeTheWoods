fa7162648e5cefcb2f555ecbc84f9f10b30ef7e8
Sydney McGreal <mcgreasm@miamioh.edu>
2024-11-03 15:33:25 -0500

    food comments

--- src/main/java/org/etw/EscapeTheWoods/Food.java
+++ src/main/java/org/etw/EscapeTheWoods/Food.java
@@ -1,10 +1,26 @@
 package org.etw.EscapeTheWoods;
-
+/**
+ * Class: Food
+ * @author Sydney McGreal
+ * @version 1.0
+ * Course: CSE 201 Spring 2024
+ * Written: October 31, 2024
+ * 
+ * Purpose: This class represents the Food class for the Escape the Woods project
+ */
 public class Food extends Item{
 
+    //attribute
     private int quantity;
 
 
+    /**
+     * Food Constructor
+     * @param name
+     * @param description
+     * @param rarity
+     * @param quantity
+     */
     public Food(String name, String description, int rarity, int quantity){
         super(name,description,rarity);
         this.quantity = quantity;
