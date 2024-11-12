bbf855e47554781527c0a5c2299aac907bc7c8a6
Brandon Belfiore <belfiobj@miamioh.edu>
2024-11-12 18:26:29 -0500

    Added determineColor method

--- src/main/java/org/etw/EscapeTheWoods/BerryBush.java
+++ src/main/java/org/etw/EscapeTheWoods/BerryBush.java
@@ -5,7 +5,8 @@
  * Course: CSE 201 Spring 2024
  * Written: November 12, 2024
  * 
- * 
+ * Purpose: A room of the scenerio where the player finds a berry bush,
+ * and can choose to eat from it or not.
  */
 
 package org.etw.EscapeTheWoods;
@@ -16,4 +17,23 @@ public class BerryBush extends Room {
     public BerryBush(double difficulty,String description, String roomName) {
         super(1, "You have found a berry bush.", "Berry Bush");
     }
+
+    public String determineColor() {
+        String color;
+        int option = (int) Math.random();
+        switch(option) {
+            case 1:
+            color = "red";
+            return color;
+            case 2:
+            color = "blue";
+            return color;
+            case 3:
+            color = "white";
+            return color;
+            default:
+
+        }
+    }
+
 }
