0e7d29068a03aa20ca4f41860d726d2037554a84
paysonb12 <150734509+paysonb12@users.noreply.github.com>
2024-11-19 16:28:30 -0500

    Update ForkInPath.java
    started on forkinpath. starting with player choosing left

--- src/main/java/org/etw/EscapeTheWoods/ForkInPath.java
+++ src/main/java/org/etw/EscapeTheWoods/ForkInPath.java
@@ -3,10 +3,26 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class ForkInPath {
+    private Player player;
 
     // constructor
-    public ForkInPath (){
-
+    public ForkInPath (Player player){
+        private Player player;
     }
-    
+
+    public void ecounterFork(Scanner scanner) {
+        System.out.println("you have entered a room with a fork in the path.");
+        System.out.println("you see a path splitting into two directions: Left and Right.");
+        System.out.println("which direction would you like to go? (left/right)");
+
+        String direction = scanner.nextLine().toLowerCase();
+        if (direction.equals("Left")) { 
+            //chooses left
+            System.out.println("you chose to go left. You lose some stamina.");
+            player.setStamina(player.getStamina() - 10); 
+            checkStamina();
+        } else if (direction.equals("right")) { 
+        }
+    }
 }
+        
